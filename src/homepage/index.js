f9f2487caecb61ef8c59123f65cf39aec29c8a04
Beto23 <josee-45@hotmail.com>
2016-06-15 23:26:52 -0500

    Agrear request superagent y cambiar header

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,34 +2,23 @@ var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
+var request = require('superagent');
+var header = require('../header');
 
-page('/', function (ctx, next) {
+page('/', header, loadPictures, function (ctx, next) {
   title('Platzigram');
   var main = document.getElementById('main-container');
 
-  var pictures = [
-    {
-      user: {
-        username: 'beto23',
-        avatar: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/1185631_627964767253159_486476512_n.jpg?oh=dbe01a246b201026a38bfd9737847d8e&oe=580DF6EC'
-      },
-      url: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/1014210_546510868731883_2130914507_n.jpg?oh=a2ef25f9a4257a0d3b65deba123f02ac&oe=5809AE5A',
-      likes: 0,
-      liked: false,
-      createdAt: new Date()
-    },
-    {
-      user: {
-        username: 'beto23',
-        avatar: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/1185631_627964767253159_486476512_n.jpg?oh=dbe01a246b201026a38bfd9737847d8e&oe=580DF6EC'
-      },
-      url: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/1014210_546510868731883_2130914507_n.jpg?oh=a2ef25f9a4257a0d3b65deba123f02ac&oe=5809AE5A',
-      likes: 1,
-      liked: true,
-      createdAt: new Date().setDate(new Date().getDate() - 10)
+  empty(main).appendChild(template(ctx.pictures));
+})
 
-    }
-  ];
+function loadPictures(ctx, next) {
+  request
+    .get('/api/pictures')
+    .end(function (err, res) {
+      if (err) return console.log(err);
 
-  empty(main).appendChild(template(pictures));
-});
+      ctx.pictures = res.body;
+      next();
+    })
+}
