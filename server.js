97a463b1a3b159d630c217d6ef1c5aa486e367e7
Beto23 <josee-45@hotmail.com>
2016-06-15 23:23:40 -0500

    Agregar api pictures

--- server.js
+++ server.js
@@ -20,6 +20,35 @@ app.get('/signin', function(req, res){
   res.render('index', { title: 'Platzigram - Signin'});
 });
 
+app.get('/api/pictures', function (req, res, next) {
+  var pictures = [
+    {
+      user: {
+        username: 'slifszyc',
+        avatar: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/1185631_627964767253159_486476512_n.jpg?oh=e040f41795fe069cba37488f5e497318&oe=57E669EC'
+      },
+      url: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/1544966_607696085946694_1721555721_n.jpg?oh=184c3dc258b9b0386c9aed91512ca272&oe=57C19AAB',
+      likes: 0,
+      liked: false,
+      createdAt: new Date().getTime()
+    },
+    {
+      user: {
+        username: 'slifszyc',
+        avatar: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/1185631_627964767253159_486476512_n.jpg?oh=e040f41795fe069cba37488f5e497318&oe=57E669EC'
+      },
+      url: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/1544966_607696085946694_1721555721_n.jpg?oh=184c3dc258b9b0386c9aed91512ca272&oe=57C19AAB',
+      likes: 1,
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate() - 10)
+    }
+  ];
+
+  setTimeout(function () {
+    res.send(pictures);
+  }, 2000);
+});
+
 app.listen(3000, function(err){
   if (err) return console.log('Hubo un error'), process.exit(1);
 
